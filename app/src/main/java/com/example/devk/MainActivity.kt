8a8d3d10775bad659ed01a32e28bd47c4d50206f
wesley oliveira <wlwwesley9@gmail.com>
2022-11-15 10:34:01 -0300

    iniciando integração com o fragment

--- app/src/main/java/com/example/devk/MainActivity.kt
+++ app/src/main/java/com/example/devk/MainActivity.kt
@@ -3,10 +3,24 @@ package com.example.devk
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onNavigateUp()
     }
 }
