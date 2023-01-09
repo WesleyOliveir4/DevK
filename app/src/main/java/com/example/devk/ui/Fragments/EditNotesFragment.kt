85807942021b89a8b6a4e09ed0bd6e3378ff3286
wesley oliveira <wlwwesley9@gmail.com>
2023-01-09 20:10:14 -0300

    edit finalizado 2:04

--- app/src/main/java/com/example/devk/ui/Fragments/EditNotesFragment.kt
+++ app/src/main/java/com/example/devk/ui/Fragments/EditNotesFragment.kt
@@ -1,60 +1,110 @@
 package com.example.devk.ui.Fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
+import com.example.devk.Model.Notes
 import com.example.devk.R
+import com.example.devk.ViewModel.NotesViewModel
+import com.example.devk.databinding.FragmentCreateNotesBinding
+import com.example.devk.databinding.FragmentEditNotesBinding
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class EditNotesFragment: Fragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [EditNotesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class EditNotesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    val oldNotes by navArgs<EditNotesFragmentArgs>()
+    lateinit var  binding: FragmentEditNotesBinding
+    val viewModel: NotesViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    var priority: String = "1"
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_notes, container, false)
+        binding = FragmentEditNotesBinding.inflate(layoutInflater , container, false)
+
+        binding.edtTitle.setText(oldNotes.data.title)
+        binding.edtSubTitle.setText(oldNotes.data.subTitle)
+        binding.edtNotes.setText(oldNotes.data.notes)
+
+
+        when(oldNotes.data.priority){
+            "1"->{
+                applyPriorityColor(binding.optEditGreen)
+                priority = "1"
+            }
+            "2"->{
+                applyPriorityColor(binding.optEditYellow)
+                priority = "2"
+            }
+            "3"->{
+                applyPriorityColor(binding.optEditRed)
+                priority = "3"
+            }
+        }
+
+        binding.optEditGreen.setOnClickListener {
+            applyPriorityColor(binding.optEditGreen)
+            priority = "1"
+        }
+        binding.optEditYellow.setOnClickListener {
+            applyPriorityColor(binding.optEditYellow)
+            priority = "2"
+        }
+        binding.optEditRed.setOnClickListener {
+            applyPriorityColor(binding.optEditRed)
+            priority = "3"
+        }
+
+
+        binding.btnEditSaveNotes.setOnClickListener{
+            updateNotes(it)
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EditNotesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EditNotesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun updateNotes(it: View?) {
+
+        val title = binding.edtTitle.text.toString()
+        val subTitle = binding.edtSubTitle.text.toString()
+        val notes = binding.edtNotes.text.toString()
+
+        val d = Date()
+        val notesDate: CharSequence = DateFormat.format("d MMMM yyyy", d.time)
+
+        val notesFinish = Notes(
+            oldNotes.data.id,
+            title = title,
+            subTitle = subTitle,
+            notes = notes,
+            date = notesDate as String,
+            priority = priority
+        )
+
+        viewModel.uptadeNotes(notesFinish)
+
+        Toast.makeText(activity,"Documento salvo", Toast.LENGTH_SHORT).show()
+
+        Navigation.findNavController((it!!)).navigate(R.id.action_editNotesFragment_to_homeFragment)
+
+
+    }
+
+    private fun applyPriorityColor (imgView: ImageView) {
+        binding.optEditGreen.setImageResource(0)
+        binding.optEditYellow.setImageResource(0)
+        binding.optEditRed.setImageResource(0)
+        imgView.setImageResource(R.drawable.ic_check)
     }
 }
