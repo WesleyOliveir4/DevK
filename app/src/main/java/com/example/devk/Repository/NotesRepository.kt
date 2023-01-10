bf115d4628eb1da34eed86fe49326f9b7f20e0c1
wesley oliveira <wlwwesley9@gmail.com>
2023-01-09 21:11:28 -0300

    filtro home finalizado 2:17

--- app/src/main/java/com/example/devk/Repository/NotesRepository.kt
+++ app/src/main/java/com/example/devk/Repository/NotesRepository.kt
@@ -9,6 +9,12 @@ class NotesRepository(val dao: NotesDao) {
         return dao.getNotes()
     }
 
+    fun getHighNotes():LiveData<List<Notes>> =dao.getHighNotes()
+
+    fun getMediumNotes():LiveData<List<Notes>> =dao.getMediumNotes()
+
+    fun getLowNotes():LiveData<List<Notes>> =dao.getLowNotes()
+
     fun insertNotes(notes: Notes){
         dao.insertNotes(notes)
     }
