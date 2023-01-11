067373813200a08ce7ae39ab98639c5d8545da4f
wesley oliveira <wlwwesley9@gmail.com>
2023-01-11 20:55:42 -0300

    Ajustes Layout

--- app/src/main/java/com/example/devk/ui/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/devk/ui/Fragments/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.GridLayoutManager
@@ -76,6 +77,8 @@ class HomeFragment : Fragment() {
 
         return binding.root
     }
+
+
 
 
 }
