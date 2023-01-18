17859c793e58e969e3d9642b66b41e95833c78a4
wesley oliveira <wlwwesley9@gmail.com>
2023-01-17 21:44:25 -0300

    exportar no menu

--- app/src/main/java/com/example/devk/ui/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/devk/ui/Fragments/HomeFragment.kt
@@ -1,18 +1,22 @@
 package com.example.devk.ui.Fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.view.*
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.view.WindowManager
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.devk.Model.Notes
 import com.example.devk.R
 import com.example.devk.ViewModel.NotesViewModel
 import com.example.devk.databinding.FragmentHomeBinding
 import com.example.devk.ui.Adapter.NotesAdapter
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import java.io.File
 
 
 class HomeFragment : Fragment() {
@@ -27,6 +31,7 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
 
         binding = FragmentHomeBinding.inflate(layoutInflater, container,false)
+        setHasOptionsMenu(true)
 
         viewModel.getNotes().observe(viewLifecycleOwner) { notesList ->
 
@@ -78,7 +83,63 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_exportar, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.title == "Exportar"){
+            val bottomSheet: BottomSheetDialog = BottomSheetDialog(requireContext(),R.style.BottomSheetStyle)
+            bottomSheet.setContentView(R.layout.dialog_export)
+
+            val textviewYes=bottomSheet.findViewById<TextView>(R.id.dialog_yes)
+            val textviewNo=bottomSheet.findViewById<TextView>(R.id.dialog_no)
+
+
+            viewModel.getNotes().observe(viewLifecycleOwner){notesList ->
+
+                textviewYes?.setOnClickListener{
+                    writeToFile(notesList,requireContext())
+                    bottomSheet.dismiss()
+
+                }
+            }
+
+            textviewNo?.setOnClickListener{
+                bottomSheet.dismiss()
+            }
+
+            bottomSheet.show()
+        }else{
+            requireActivity().onBackPressed()
+        }
+
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun writeToFile(data: List<Notes>, context: Context) {
+
+//        val path = Environment.getExternalStoragePublicDirectory(
+//            Environment.DIRECTORY_DOCUMENTS
+//        )
+//        val file = File(path, "DataBaseDevK.txt")
+
+        try {
+//            if (!file.exists()) {
+//                File.createTempFile("DataBaseDevK",".txt",path);
+//            }
+//
+//
+        //   file.writeText()
+
+        }
+        catch (e: Exception) {
+            Log.e("Exception", "Falha na exportação: " + e.toString());
+        }
+    }
 
 
 }
